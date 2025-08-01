a3f1278f4fd2220ecb52222926b783d88551d893
mdayan13 <mdayan1qaz@gmail.com>
2025-08-01 12:06:19 +0000

    .

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,9 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
+import {registerLicense} from "@syncfusion/ej2-base";
+
+registerLicense(import.meta.env.VITE_SYNC_LICENSE_KEY);
 
 import type { Route } from "./+types/root";
 import "./app.css";
